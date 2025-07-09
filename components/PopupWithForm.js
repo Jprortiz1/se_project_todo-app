90ae752a7cd6f99428b5e04e37c3e623de30baca
Jorge Proano <j.prortiz1@gmail.com>
2025-07-09 18:00:20 -0400

    8project.1

---
+++ components/PopupWithForm.js
@@ -0,0 +1,33 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._form = this._popup.querySelector("form");
+    this._handleFormSubmit = handleFormSubmit;
+    this._inputList = Array.from(this._form.querySelectorAll("input"));
+    this._submitButton = this._form.querySelector("button[type='submit']");
+  }
+
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach((input) => {
+      formValues[input.name] = input.value;
+    });
+    return formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      const inputValues = this._getInputValues();
+      this._handleFormSubmit(inputValues);
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,41 @@
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import FormValidator from "../components/FormValidator.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import Section from "../components/Section.js";
+import TodoCounter from "../components/TodoCounter.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
-// Elementos del DOM
+// DOM
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopup = document.querySelector("#add-todo-popup");
-const addTodoForm = document.forms["add-todo-form"];
-const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-const todosList = document.querySelector(".todos__list");
+const counterSelector = ".counter__text";
 
-// Abrir popup
-const openModal = (modal) => {
-  modal.classList.add("popup_visible");
-};
+// Contador
+const todoCounter = new TodoCounter(initialTodos, counterSelector);
 
-// Cerrar popup
-const closeModal = (modal) => {
-  modal.classList.remove("popup_visible");
-};
-
-// Generar una tarjeta de tarea usando la clase Todo
-const generateTodo = (data) => {
+// Crear tarea visual
+const createTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   return todo.getView();
 };
 
-// Renderizar un ToDo y añadirlo a la lista (al inicio o al final)
-const renderTodo = (item, prepend = false) => {
-  const todo = generateTodo(item);
-  if (prepend) {
-    todosList.prepend(todo);
-  } else {
-    todosList.append(todo);
-  }
-};
+// Sección de tareas
+const todoSection = new Section(
+  {
+    items: initialTodos,
+    renderer: (item) => {
+      const todoElement = createTodo(item);
+      todoSection.addItem(todoElement);
+    },
+  },
+  ".todos__list"
+);
+todoSection.renderItems();
 
-// Instancia de la clase FormValidator y activación de la validación
-const validator = new FormValidator(validationConfig, addTodoForm);
-validator.enableValidation();
-
-// Escuchar botón de agregar tarea
-addTodoButton.addEventListener("click", () => {
-  openModal(addTodoPopup);
-});
-
-// Escuchar botón de cerrar el popup
-addTodoCloseBtn.addEventListener("click", () => {
-  closeModal(addTodoPopup);
-});
-
-// Escuchar el envío del formulario para agregar un nuevo to-do
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  const name = addTodoForm.elements["name"].value;
-  const dateInput = addTodoForm.elements["date"].value;
-
-  // Ajuste de la fecha por zona horaria
+// Popup
+const addTodoPopup = new PopupWithForm("#add-todo-popup", (formData) => {
+  const name = formData["name"];
+  const dateInput = formData["date"];
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
@@ -68,12 +46,41 @@ addTodoForm.addEventListener("submit", (evt) => {
     id: uuidv4(),
   };
 
-  renderTodo(newTodo, true); // Añadir al inicio
-  closeModal(addTodoPopup);
-  validator.resetValidation(); // Ya incluye limpiar el formulario
+  const todoElement = createTodo(newTodo);
+  todoSection.addItem(todoElement);
+  updateCounter();
+  addTodoPopup.close();
+});
+addTodoPopup.setEventListeners();
+
+addTodoButton.addEventListener("click", () => {
+  addTodoPopup.open();
 });
 
-// Renderizar los to-dos iniciales al cargar la página
-initialTodos.forEach((item) => {
-  renderTodo(item);
+// Validación
+const formElement = document.forms["add-todo-form"];
+const formValidator = new FormValidator(validationConfig, formElement);
+formValidator.enableValidation();
+
+// Delegación de eventos
+document.addEventListener("change", (e) => {
+  if (e.target.classList.contains("todo__completed")) {
+    updateCounter();
+  }
 });
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("todo__delete-btn")) {
+    setTimeout(updateCounter, 0);
+  }
+});
+
+// Actualizar contador
+function updateCounter() {
+  const total = document.querySelectorAll(".todo").length;
+  const completed = document.querySelectorAll(
+    ".todo__completed:checked"
+  ).length;
+  todoCounter._total = total;
+  todoCounter._completed = completed;
+  todoCounter._updateText();
+}
